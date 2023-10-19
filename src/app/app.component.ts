e3f9101dcf395a2cb13372d6f61cb213bcd94c6a
Ankita Kar <akar@argusoft.com>
2023-10-19 12:20:15 +0530

    fresh folder

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,13 @@
-import { Component, ViewChild, AfterViewInit } from '@angular/core';
-import { PostComponent } from './post/post.component';
+import { Component } from '@angular/core';
+
 @Component({
 
   selector: 'app-root',
-  
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit{
-  title = 'AngCourse';
 
-  parentMessage:string = "message from parent component (changed)"
-
-  message!:string;
-
-
-  fromChildOutput!:string;
-
-  @ViewChild(PostComponent) childComp: any;
-
-  constructor(){
-    console.log(this.childComp);
-    
-  }
-
-  ngAfterViewInit(): void {
-      console.log(this.childComp);
-
-      this.message = this.childComp.childMessage;
-      
-  }
-
-  recieveMessage($event: any){
-        this.fromChildOutput = $event
-  }
+export class AppComponent {
+  
 
 }
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
-import { EventManager } from '@angular/platform-browser';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post',
@@ -8,37 +7,9 @@ import { EventManager } from '@angular/platform-browser';
 })
 
 
-export class PostComponent implements OnInit{
+export class PostComponent {
 
-  title: string = "List of Posts"
-
-  messagePost:string  = "Message Post"
-  
-  parentPostMessage:string = "message from parent post component"
-  
-  childMessage:string = "from child post component"
-  
-  outputChildMessage:string = "message from child part 2"
-  
-  @Input()
-  fromParent!: string;
-
-  @Output()
-  messageEvent = new EventEmitter<string>()
-
-  constructor(){}
-
-  ngOnInit(): void {
-      
-  }
-
-  sendMessage(){
-    // console.log('click');
-
-    this.messageEvent.emit(this.outputChildMessage);
-    
-  }
+ 
 
 }
 
-//ngOnInit() is a callback lifecycle method that gets invoked after the particular component gets fully initialized inisde the DOM (fully loaded inside the browser)
